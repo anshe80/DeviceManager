9ad8a2debf126d6c5ed0c3fc478bb2b54beb624d
clin <605370259@qq.com>
2017-05-21 13:15:01 +0800

    Remerge rackmonkey code into my project

--- conf/config.sh
+++ conf/config.sh
@@ -1,5 +1,10 @@
 #!/usr/bash
 
+sudo apt-get install sqlite3
+sudo apt-get install libdbi-perl
+sudo apt-get install libdbd-sqlite3-perl
+sudo apt-get install libhtml-template-perl
+sudo apt-get install libhtml-parser-perl
 
 rootPath=$(pwd)
 sudo rm -rf /etc/rackmonkey.conf
@@ -8,19 +13,19 @@ sudo ln -s -d $rootPath/rackmonkey.conf /etc/rackmonkey.conf
 sudo rm -rf /etc/apache2/conf.d/httpd-rackmonkey.conf
 sudo ln -s $rootPath/httpd-rackmonkey.conf /etc/apache2/conf.d/httpd-rackmonkey.conf
 
-cd ../..
+cd ..
 rootPath=$(pwd)
-cd -
+#cd -
 
 sudo rm -rf /var/www/rackmonkey
 
-sudo ln -s -d $rootPath/DeviceManager /var/www/rackmonkey
+sudo ln -s -d $rootPath/src /var/www/rackmonkey
 
-cd ..
-rootPath=$(pwd)
+#cd ..
+#rootPath=$(pwd)
 
-sudo chmod 755 rackmonkey.pl
-cd -
+sudo chmod 755 src/rackmonkey.pl
+#cd -
 
 sudo mkdir /var/lib/rackmonkey
 sudo chown www-data:www-data /var/lib/rackmonkey
@@ -31,6 +36,18 @@ sudo ln -s -d $rootPath/tmpl /var/lib/rackmonkey/tmpl
 # force reload apache2
 sudo /etc/init.d/apache2 force-reload
 
-cd ..
-rootPath=$(pwd)
-sudo chown www-data:www-data $rootPath/rackmonkey.db
+#cd ..
+#rootPath=$(pwd) 
+
+sqlite3 ./rackmonkey.db < sql/schema/schema.sqlite.sql
+sqlite3 ./rackmonkey.db < sql/data/default_data.sql
+sqlite3 ./rackmonkey.db < sql/data/sample_data.sql
+sudo ln -s -d $rootPath/rackmonkey.db /var/lib/rackmonkey/rackmonkey.db
+sudo chown www-data:www-data /var/lib/rackmonkey/rackmonkey.db
+
+echo "********** Follows are creating symbolic **********"
+ls /var/lib/rackmonkey
+ls /etc/apache2/conf.d/httpd-rackmonkey.conf
+ls /etc/rackmonkey.conf
+
+
